2df305b099a929ee3588e74f5f4dea987be3a543
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-05-24 12:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+
+window.onload = function() {
+    if(localStorage.getItem('username') && localStorage.getItem('password')) {
+        var btn = document.getElementById('existing');
+        btn.style.display = "block";
+        btn.onclick = function() {
+            alert('Logged in as ' + localStorage.getItem('username'));
+        };
+    }
+};
+
+function submitForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var remember = document.getElementById('checkbox').checked;
+
+    if(remember) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+
+    alert('Logged in as ' + username);
+}
